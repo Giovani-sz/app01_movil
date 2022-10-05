2830139d164fd0c55f3fe7e5f07fcdf0ca3f0156
giovani <giovaniszg@gmail.com>
2022-10-05 13:33:24 -0500

    agregado de iconos

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,8 @@ Widget build(BuildContext context) {
               color: Colors.white,
               fontSize: 25.0,
               fontWeight: FontWeight.w500,
+              fontFamily: "Dancing",
+
               
               ),
             ),
@@ -36,7 +38,66 @@ Widget build(BuildContext context) {
                 letterSpacing: 2.0,
 
                  ),
-            )
+            ),
+            Divider(
+              thickness: 0.70,
+              color: Colors.white38,
+              indent: 120.0,
+              endIndent: 120.0,
+            ),
+            Card(
+              color: Colors.white,
+              margin:EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
+              elevation: 20.0,
+            child:ListTile(
+              title: Text("+51 65898554"),
+              subtitle:  Text("Telefono"),
+              leading: Icon(Icons.phone,
+              color: Colors.indigo,
+              ),
+              trailing: Icon(Icons.check_circle_outline,
+              color: Colors.indigo,
+              ),
+            ),
+            ),
+            Card(
+              color: Colors.white,
+              margin:EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
+              elevation: 20.0,
+            child:ListTile(
+              title: Text("eduardo@gmail.com"),
+              subtitle:  Text("Correo  Electronico"),
+              leading: Icon(Icons.mail,
+              color: Colors.indigo,
+              ),
+              trailing: Icon(Icons.check_circle_outline,
+              color: Colors.indigo,
+              ),
+            ),
+            ),
+            Row(children: [
+              Icon(Icons.facebook,
+              color: Colors.white,
+              size: 90.0,
+              ),
+              SizedBox(
+              width: 50.0,
+              ),
+              Icon(Icons.alarm,
+              color: Colors.white,
+              size: 90.0,
+              ),
+              SizedBox(
+              width: 50.0,
+              ),
+              Icon(Icons.share,
+              color: Colors.white,
+              size: 90.0,
+              ),
+              SizedBox(
+              width: 10.0,
+              ),
+            ])
         ],
         )
     ),
